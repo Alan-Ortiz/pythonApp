c8e9b118c2e0df24a7e857af5907e42291ce196b
Alan-Ortiz <alanrcz93@gmail.com>
2019-06-02 10:15:11 -0500

    Update app-deploy.sh

--- app-deploy.sh
+++ app-deploy.sh
@@ -1,5 +1,4 @@
-
 managepath=`find . -name manage.py`
-python3 $managepath makemigrations myApp
-python3 $managepath migrate myApp
+python3 $managepath makemigrations myapp
+python3 $managepath migrate myapp
 nohup python3 $managepath runserver &
