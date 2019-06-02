45e850052f395887ae3f40f174938ba3da10e0be
Alan-Ortiz <alanrcz93@gmail.com>
2019-06-02 11:06:15 -0500

    Update app-deploy.sh

--- app-deploy.sh
+++ app-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd '/app'
-MP=`sudo find $(pwd) -name manage.py`
+MP=`find $(pwd) -name manage.py`
 echo $MP
-AP=`sudo find $(pwd) -name myapp`
+AP=`find $(pwd) -name myapp`
 echo $AP
 echo "$(dirname -- "$AP")"
 cd $AP
 python3 $MP makemigrations myapp
 python3 $MP migrate myapp
-nohup python3 $managepath runserver && echo "finished"
+nohup python3 $MP runserver && echo "finished"
 exit 0
